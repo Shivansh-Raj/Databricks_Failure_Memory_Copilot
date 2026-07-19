b5eca4839d306b90c95ec625255a47b790109d52
Shivansh Raj <shivanshrajdehl@gmail.com>
2026-07-19 16:55:24 +0530

    readme updated

--- UI/streamlit_app.py
+++ UI/streamlit_app.py
@@ -13,7 +13,7 @@ API_BASE = "http://localhost:8000"
 # --------------------------------------------------------------------------- #
 
 st.set_page_config(
-    page_title="Databricks Failure Copilot",
+    page_title="Failure Memory Copilot",
     page_icon="🔥",
     layout="wide",
 )
